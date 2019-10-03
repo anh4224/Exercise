a35ae5e0afc72df43eec5730359697f564b4d8b6
tuquanganh <bantia447@gmail.com>
2019-10-03 22:13:05 +0700

    Exercise4b

---
+++ Exercise4b/Exercise4b/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4b
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            InputValues(array);
+        }
+        public static void InputValues(int[] iArr)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                string str = Console.ReadLine();
+                iArr[i] = int.Parse(str);
+
+            }
+
+        }
+        public static bool IsContain5(int[] iArr)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                if (iArr[i] == 5)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public static bool IsContain5or6(int[] iArr)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                if (iArr[i] == 5 || iArr[i] == 6 || iArr[i] == 5 && iArr[i] == 6)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+
+        }
+        public static bool IsContain5and6(int[] iArr)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                for (int j = 0; j < iArr.Length; j++)
+                {
+                    if (iArr[i] == 5 && iArr[j] == 6)
+
+                        return true;
+                }
+            }
+            return false;
+        }
+        public static int Count5(int[] iArr)
+        {
+            int count = 0;
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                if (iArr[i] == 5)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int Count5or6(int[] iArr)
+        {
+            int count1 = 0;
+            for (int i = 0; i < iArr.Length; i++)
+            {
+
+                if (iArr[i] == 5 || iArr[i] == 6)
+                    count1++;
+            }
+            return count1;
+        }
+        public static int Sum(int[] iArr)
+        {
+            int sum = 0;
+            for (int i = 0; i < iArr.Length; i++)
+            {
+
+
+                sum += iArr[i];
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ Exercise4b/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise4b;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] Arr1 = new int[] { 1, 2, 3, 4, 5 };
+            bool result1 = Program.IsContain5(Arr1);
+            Assert.AreEqual(true, result1);
+
+            int[] Arr2 = new int[] { 1, 82, 3, 4, 6 };
+            bool result2 = Program.IsContain5(Arr2);
+            Assert.AreEqual(false, result2);
+
+            int[] Arr3 = new int[0];
+            bool result3 = Program.IsContain5(Arr3);
+            Assert.AreEqual(false, result3);
+        }
+        [TestMethod]
+        public void TestContain5or6()
+        {
+            int[] Arr1 = new int[] { 1, 5, 7, 8, 9 };
+            bool result1 = Program.IsContain5or6(Arr1);
+            Assert.AreEqual(true, result1);
+
+            int[] Arr2 = new int[] { 1, 3, 7, 8, 9 };
+            bool result2 = Program.IsContain5or6(Arr2);
+            Assert.AreEqual(false, result2);
+
+            int[] Arr3 = new int[] { 1, 2, 6, 7, 8, 9 };
+            bool result3 = Program.IsContain5or6(Arr3);
+            Assert.AreEqual(true, result3);
+        }
+        [TestMethod]
+        public void TestContain5and6()
+        {
+            int[] Arr = new int[] { 1, 0, 7, 8, 9 };
+            bool result1 = Program.IsContain5and6(Arr);
+            Assert.AreEqual(false, result1);
+
+            int[] Arr4 = new int[] { 1, 5, 7, 8, 9 };
+            bool result4 = Program.IsContain5and6(Arr4);
+            Assert.AreEqual(false, result4);
+
+            int[] Arr2 = new int[] { 1, 6, 7, 8, 9 };
+            bool result2 = Program.IsContain5and6(Arr2);
+            Assert.AreEqual(false, result2);
+
+            int[] Arr3 = new int[] { 1, 5, 6, 7, 8, 9 };
+            bool result3 = Program.IsContain5and6(Arr3);
+            Assert.AreEqual(true, result3);
+        }
+        [TestMethod]
+        public void TestCount5()
+        {
+            int[] Arr1 = new int[] { 1, 0, 6, 4, 2 };
+            int result1 = Program.Count5(Arr1);
+            Assert.AreEqual(0, result1);
+
+            int[] Arr2 = new int[] { 1, 0, 5, 6, 4, 2 };
+            int result2 = Program.Count5(Arr2);
+            Assert.AreEqual(1, result2);
+
+            int[] Arr3 = new int[] { 1, 0, 5, 5, 6, 4, 2 };
+            int result3 = Program.Count5(Arr3);
+            Assert.AreEqual(2, result3);
+        }
+        [TestMethod]
+        public void TestCount5or6()
+        {
+            int[] Arr1 = new int[] { 1, 0, 6, 4, 2 };
+            int result1 = Program.Count5or6(Arr1);
+            Assert.AreEqual(1, result1);
+
+            int[] Arr2 = new int[] { 1, 0, 6, 6, 4, 2 };
+            int result2 = Program.Count5or6(Arr2);
+            Assert.AreEqual(2, result2);
+
+            int[] Arr3 = new int[] { 1, 0, 5, 6, 4, 2 };
+            int result3 = Program.Count5or6(Arr3);
+            Assert.AreEqual(2, result3);
+        }
+        [TestMethod]
+        public void TestSum()
+        {
+            int[] Arr1 = new int[] { 1, 2 };
+            int result1 = Program.Sum(Arr1);
+            Assert.AreEqual(3, result1);
+
+            int[] Arr2 = new int[] { 2, 2, 2 };
+            int result2 = Program.Sum(Arr2);
+            Assert.AreEqual(6, result2);
+
+            int[] Arr3 = new int[] { 0, 0, 0 };
+            int result3 = Program.Sum(Arr3);
+            Assert.AreEqual(0, result3);
+
+        }
+    }
+}
+
